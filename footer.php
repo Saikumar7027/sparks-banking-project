dcbcb16887de1f59f133a9b82cc33f1df740ec73
Saikumar7027 <88276852+Saikumar7027@users.noreply.github.com>
2021-08-15 19:11:36 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">© Made by sai 2021 <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('nav.php') ?>
+
+
+  <h1>Welcome to Sparking Bank INDIA</h1>
+
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">VIEW ALL USERS</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">TRANSFER MONEY</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">VIEW TRANSACTION HISTORY</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
